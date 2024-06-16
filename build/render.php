985699a57e8b880872734c52e1207aa1a8a9674f
Antonio Molina ordoñez <amolinao@opentrends.net>
2024-06-16 22:26:32 +0200

    Adding init of project

---
+++ build/render.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<p <?php echo get_block_wrapper_attributes(); ?>>
+	<?php esc_html_e( 'Amolina Wp Block Seed – hello from a dynamic block!', 'amolina-wp-block-seed' ); ?>
+</p>
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd1bbfd9a1b9df6a521db');
